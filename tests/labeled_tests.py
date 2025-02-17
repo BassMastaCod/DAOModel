66076680142403d9de6086410ba386260eb8debc
Cod <bassmastacod@gmail.com>
2025-02-16 23:29:16 -0500

    labeled tests
    Updated DAOModel tests to use the new labeled_tests utility. This cleans up the code while providing the benefits mentioned in commit where utility function was introduced. Added some additional test cases now that doing so is easily done. Some additional functionality and bug fixes were implemented to labeled_tests to support the new test scenarios.

--- tests/labeled_tests.py
+++ tests/labeled_tests.py
@@ -4,14 +4,19 @@ from typing import Callable, Union, Any
 import pytest
 
 
-def labeled_tests(test_case_data: dict[str, Union[tuple[Any, ...], list[tuple[Any, ...]]]]):
+def labeled_tests(test_case_data: dict[str, Union[tuple[Any, ...], list[Union[tuple[Any, ...], Any]]]]):
     def decorator(test_func: Callable):
-        params = ", ".join(inspect.signature(test_func).parameters.keys())
+        params = inspect.signature(test_func).parameters.keys()
         labels = []
         test_data = []
         for key, value in test_case_data.items():
             for data in value if isinstance(value, list) else [value]:
                 labels.append(key)
+                if len(params) > 1:
+                    if not isinstance(data, tuple):
+                        raise ValueError(f'Expected tuple of {len(params)} parameters but got {data}')
+                    elif len(data) != len(params):
+                        raise ValueError(f'Expected {len(params)} parameters but got {len(data)}: ({data})')
                 test_data.append(data)
-        return pytest.mark.parametrize(params, test_data, ids=labels)(test_func)
+        return pytest.mark.parametrize(', '.join(params), test_data, ids=labels)(test_func)
     return decorator
