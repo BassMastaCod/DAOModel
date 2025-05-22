0dd9f96acffb400eeb3270cf9c6011cd38e42f34
Cody M Sommer <BassMastaCod@gmail.com>
2025-05-22 16:02:40 -0400

    Add PropertyFilter classes to use logical operators for property gathering.
    Introduced a `PropertyFilter` class hierarchy to filter properties based on AND, OR, NOT, and custom conditions. Included unit tests to validate filter functionality, ensuring correct evaluation and operator overloading support.

---
+++ tests/test_property_filter.py
@@ -0,0 +1,222 @@
+from typing import Optional, Any
+
+import pytest
+from sqlmodel import Field
+
+from daomodel import DAOModel
+from daomodel.property_filter import PropertyFilter, BasicPropertyFilter, AndFilter, ALL, PK, FK, DEFAULT, NONE
+from tests.labeled_tests import labeled_tests
+
+
+class ForeignModel(DAOModel, table=True):
+    pk: int = Field(primary_key=True)
+
+
+class PropertyModel(DAOModel, table=True):
+    pk_value: int = Field(primary_key=True)
+    pk_default_none: Optional[int] = Field(primary_key=True)
+    pk_default: int = Field(default=0, primary_key=True)
+    pk_override: int = Field(default=0, primary_key=True)
+    pk_default_optional: Optional[int] = Field(default=0, primary_key=True)
+    pk_override_optional: Optional[int] = Field(default=0, primary_key=True)
+    pk_override_none: Optional[int] = Field(default=0, primary_key=True)
+
+    pk_fk_value: int = Field(primary_key=True, foreign_key="foreign_model.pk")
+    pk_fk_default_none: Optional[int] = Field(primary_key=True, foreign_key="foreign_model.pk")
+    pk_fk_default: int = Field(default=0, primary_key=True, foreign_key="foreign_model.pk")
+    pk_fk_override: int = Field(default=0, primary_key=True, foreign_key="foreign_model.pk")
+    pk_fk_default_optional: Optional[int] = Field(default=0, primary_key=True, foreign_key="foreign_model.pk")
+    pk_fk_override_optional: Optional[int] = Field(default=0, primary_key=True, foreign_key="foreign_model.pk")
+    pk_fk_override_none: Optional[int] = Field(default=0, primary_key=True, foreign_key="foreign_model.pk")
+
+    fk_value: int = Field(foreign_key="foreign_model.pk")
+    fk_default_none: Optional[int] = Field(foreign_key="foreign_model.pk")
+    fk_default: int = Field(default=0, foreign_key="foreign_model.pk")
+    fk_override: int = Field(default=0, foreign_key="foreign_model.pk")
+    fk_default_optional: Optional[int] = Field(default=0, foreign_key="foreign_model.pk")
+    fk_override_optional: Optional[int] = Field(default=0, foreign_key="foreign_model.pk")
+    fk_override_none: Optional[int] = Field(default=0, foreign_key="foreign_model.pk")
+
+    field_value: int = Field()
+    field_default_none: Optional[int] = Field()
+    field_default: int = Field(default=0)
+    field_override: int = Field(default=0)
+    field_default_optional: Optional[int] = Field(default=0)
+    field_override_optional: Optional[int] = Field(default=0)
+    field_override_none: Optional[int] = Field(default=0)
+
+
+property_model = PropertyModel(
+    pk_value=1,
+    pk_override=1,
+    pk_override_optional=1,
+    pk_override_none=None,
+
+    pk_fk_value=1,
+    pk_fk_override=1,
+    pk_fk_override_optional=1,
+    pk_fk_override_none=None,
+
+    fk_value=1,
+    fk_override=1,
+    fk_override_optional=1,
+    fk_override_none=None,
+
+    field_value=1,
+    field_override=1,
+    field_override_optional=1,
+    field_override_none=None,
+)
+
+all_properties = [
+    'pk_value',
+    'pk_default_none',
+    'pk_default',
+    'pk_override',
+    'pk_default_optional',
+    'pk_override_optional',
+    'pk_override_none',
+
+    'pk_fk_value',
+    'pk_fk_default_none',
+    'pk_fk_default',
+    'pk_fk_override',
+    'pk_fk_default_optional',
+    'pk_fk_override_optional',
+    'pk_fk_override_none',
+
+    'fk_value',
+    'fk_default_none',
+    'fk_default',
+    'fk_override',
+    'fk_default_optional',
+    'fk_override_optional',
+    'fk_override_none',
+
+    'field_value',
+    'field_default_none',
+    'field_default',
+    'field_override',
+    'field_default_optional',
+    'field_override_optional',
+    'field_override_none'
+]
+
+@pytest.mark.parametrize('property_filter, expected', [
+    (ALL, all_properties),
+    (PK, {name for name in all_properties if 'pk_' in name}),
+    (FK, {name for name in all_properties if 'fk_' in name}),
+    (DEFAULT, {name for name in all_properties if 'default' in name}),
+    (NONE, {name for name in all_properties if 'none' in name})
+])
+def test_basic_filter(property_filter: BasicPropertyFilter, expected: list[str]):
+    assert property_filter.evaluate(property_model) == set(expected)
+
+@pytest.mark.parametrize('property_filter', [ALL, PK, FK, DEFAULT, NONE])
+def test_not_filter(property_filter: BasicPropertyFilter):
+    normal_filter = property_filter.evaluate(property_model)
+    not_filter = (~property_filter).evaluate(property_model)
+    assert normal_filter.isdisjoint(not_filter)
+    assert normal_filter | not_filter == set(all_properties)
+
+@pytest.mark.parametrize('property_filter', [ALL, PK, FK, DEFAULT, NONE])
+def test_or_filter(property_filter: BasicPropertyFilter):
+    assert (property_filter | ~property_filter).evaluate(property_model) == set(all_properties)
+
+@pytest.mark.parametrize('property_filter, expected', [
+    (PK & FK, {name for name in all_properties if 'pk_fk_' in name}),
+    (DEFAULT & NONE, {'pk_default_none', 'pk_fk_default_none', 'fk_default_none', 'field_default_none'}),
+    (PK & DEFAULT, {'pk_default_none', 'pk_default', 'pk_default_optional', 'pk_fk_default_none', 'pk_fk_default', 'pk_fk_default_optional'}),
+    (FK & NONE, {'pk_fk_default_none', 'pk_fk_override_none', 'fk_default_none', 'fk_override_none'}),
+])
+def test_and_filter(property_filter: AndFilter, expected: list[str]):
+    assert property_filter.evaluate(property_model) == set(expected)
+
+def test_evaluate_to_no_results():
+    assert (~ALL).evaluate(property_model) == set()
+
+@labeled_tests({
+    'modified to None':
+        (~DEFAULT & NONE, {'pk_override_none', 'pk_fk_override_none', 'fk_override_none', 'field_override_none'}),
+    'existing relationships':
+        (FK & ~NONE, {
+            'pk_fk_value',
+            'pk_fk_default',
+            'pk_fk_override',
+            'pk_fk_default_optional',
+            'pk_fk_override_optional',
+
+            'fk_value',
+            'fk_default',
+            'fk_override',
+            'fk_default_optional',
+            'fk_override_optional'
+        }),
+    'standard fields':
+        (~PK & ~FK, {name for name in all_properties if 'field_' in name}),
+    'keys':
+        (PK | FK, {
+            'pk_value',
+            'pk_default_none',
+            'pk_default',
+            'pk_override',
+            'pk_default_optional',
+            'pk_override_optional',
+            'pk_override_none',
+
+            'pk_fk_value',
+            'pk_fk_default_none',
+            'pk_fk_default',
+            'pk_fk_override',
+            'pk_fk_default_optional',
+            'pk_fk_override_optional',
+            'pk_fk_override_none',
+
+            'fk_value',
+            'fk_default_none',
+            'fk_default',
+            'fk_override',
+            'fk_default_optional',
+            'fk_override_optional',
+            'fk_override_none'
+        }),
+    'fields that are primary and non-null or foreign and default':
+        (PK & ~NONE | FK & DEFAULT, {
+            'pk_value',
+            'pk_default',
+            'pk_override',
+            'pk_default_optional',
+            'pk_override_optional',
+
+            'pk_fk_value',
+            'pk_fk_default_none',
+            'pk_fk_default',
+            'pk_fk_override',
+            'pk_fk_default_optional',
+            'pk_fk_override_optional',
+
+            'fk_default_none',
+            'fk_default',
+            'fk_default_optional'
+        }),
+    'non-keys with a modified value that is not None':
+        (~FK & ~PK & ~DEFAULT & ~NONE, {'field_value', 'field_override', 'field_override_optional'}),
+    'default fields except unset optional unless it is pk fk or not a key at all':
+        (DEFAULT & ~(NONE & DEFAULT & ~(PK & FK | ~(PK | FK))), {
+            'pk_default',
+            'pk_default_optional',
+
+            'pk_fk_default_none',
+            'pk_fk_default',
+            'pk_fk_default_optional',
+
+            'fk_default',
+            'fk_default_optional',
+
+            'field_default_none',
+            'field_default',
+            'field_default_optional',
+        })
+})
+def test_mixed_filters(property_filter: PropertyFilter, expected: set[str]):
+    assert property_filter.evaluate(property_model) == expected
