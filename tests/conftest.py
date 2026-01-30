e485c0a523e92d39ae7729435f905b6275843cf3
Cody M Sommer <BassMastaCod@gmail.com>
2026-01-30 18:52:25 -0500

    Added documentation to help debug test code

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,10 @@ from daomodel.testing import TestDAOFactory
 def daos_fixture() -> Generator[TestDAOFactory, Any, None]:
     """
     Provides a DAOFactory for Testing as a pytest fixture named `daos`.
+
+    Change to `TestDAOFactory(debug=True)` in order to write the DB to file.
+    NOTE: this avoids cleaning up test data after test execution, allowing for inspection
+        As a result will, subsequent tests will likely fail due to not having a clean DB.
     """
     with TestDAOFactory() as daos:
         yield daos
